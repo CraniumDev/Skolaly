c25bf163b700bd8d708aaf7934868dc8fa6acbc4
Blaise Fringel <ffringel9@gmail.com>
2017-04-11 17:14:40 +0100

    env variables

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -42,8 +42,7 @@ public class Application extends Controller {
         String DEFAULT_COLLECTION = System.getenv("DEFAULT_COLLECTION");
         String zkHost1 = System.getenv("zkHostString1");
         String zkHost2 = System.getenv("zkHostString2");
-        String zkHostString = "ec2-34-193-230-172.compute-1.amazonaws.com:2181," +
-                "ec2-34-194-75-157.compute-1.amazonaws.com:2181";
+        String zkHostString = zkHost1 + "," + zkHost2;
 
         CloudSolrClient solrClient = new CloudSolrClient.Builder().withZkHost(zkHostString).build();
         solrClient.setDefaultCollection(DEFAULT_COLLECTION);
