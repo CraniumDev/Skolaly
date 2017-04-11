d7ee95cabe9e34ee96c801f933b3eedc06bd2621
Blaise Fringel <ffringel9@gmail.com>
2017-04-11 14:32:39 +0100

    env variables

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -39,9 +39,9 @@ public class Application extends Controller {
     }
 
     public static void query(@Required String keyword) throws SolrServerException, IOException {
-        String DEFAULT_COLLECTION = System.getenv("DEFAULT_COLLECTION");
-        String zkHostString = System.getenv("zkHostString1") +
-                System.getenv("zkHostString2");
+        String DEFAULT_COLLECTION = "skolaly";
+        String zkHostString = "ec2-34-193-230-172.compute-1.amazonaws.com:2181," +
+                "ec2-34-194-75-157.compute-1.amazonaws.com:2181";
 
         CloudSolrClient solrClient = new CloudSolrClient.Builder().withZkHost(zkHostString).build();
         solrClient.setDefaultCollection(DEFAULT_COLLECTION);
